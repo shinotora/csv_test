8205a39c73f5adef58bc6e00ffd97b90d8ae7548
shinotora <tg2003vi@gmail.com>
2017-05-29 16:19:42 +0900

    fix

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ 
-	if
-
+if [ $# -eq 1 ]; then
+	if [ -e $1 ];then
+		echo "file $1 exists"
 	else
-	
+		echo "file $1 not found"
 	fi
 else
-
+	echo "usage: fe filename - report the exstence of a file"
 fi
